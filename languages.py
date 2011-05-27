fec0806497df9b1e5809dfba170e532ea9cf0364
Rodrigo Augosto <rodrigo.augosto@gmail.com>
2011-05-27 16:59:46 -0400

    Contact working

---
+++ languages.py
@@ -0,0 +1,43 @@
+## english dict
+
+en = {}
+en["id"] = "en"
+en["menu_1"] = "Home"
+en["menu_2"] = "Blog"
+en["menu_3"] = "About"
+en["menu_4"] = "Contact"
+
+en["choose_your_language"] = "Choose your Language"
+en["spanish"] = "Spanish"
+en["english"] = "English"
+
+en["contact_popup_title"] = "Contact"
+en["contact_popup_desc"] = "Enter the two Captcha Words and fill in the fields to send me an email"
+en["contact_popup_ok_btn"] = "Send"
+en["contact_popup_cancel_btn"] = "Cancel"
+en["message_title"] = "Message"
+en["invalid_email_address"] = "Please insert a valid email address"
+en["invalid_captcha"] = "Invalid Captcha"
+en["successfuly_sent"] = "Message successfuly sent"
+
+## spanish dict
+
+es = {}
+es["id"] = "en"
+es["menu_1"] = "Home"
+es["menu_2"] = "Blog"
+es["menu_3"] = "Acerca"
+es["menu_4"] = "Contacto"
+
+es["choose_your_language"] = "Selecciona tu idioma"
+es["spanish"] = "Espa&ntilde;ol"
+es["english"] = "Ingl&eacute;s"
+
+es["contact_popup_title"] = "Contacto"
+es["contact_popup_desc"] = "Ingresa las dos palabras Captcha y llena los campos para enviarme un email"
+es["contact_popup_ok_btn"] = "Enviar"
+es["contact_popup_cancel_btn"] = "Canceler"
+es["message_title"] = "Mensaje"
+es["invalid_email_address"] = "Por favor, ingresa un email v&aacute;lido"
+es["invalid_captcha"] = "Captcha inv&aacute;lido"
+es["successfuly_sent"] = "Mensaje satisfactoriamente enviado"
